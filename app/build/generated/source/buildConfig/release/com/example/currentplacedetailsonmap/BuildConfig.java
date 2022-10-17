32ecea47a5ac63979ece719def33ce51aae3e49c
tsaidongdong <62095802+tsaidongdong@users.noreply.github.com>
2022-10-17 11:31:14 +0800

    Add files via upload

---
+++ app/build/generated/source/buildConfig/release/com/example/currentplacedetailsonmap/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.currentplacedetailsonmap;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.currentplacedetailsonmap";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from the variant API
+  public static final String MAPS_API_KEY = "AIzaSyCrkpGMPSpno8i20ukKDGgveveF4oGy00M";
+}
